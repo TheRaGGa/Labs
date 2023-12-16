d39a7c7df9fbf34f704c2b001e037fd17f4a389e
Rashad V. Aslanli <rashadva@code.local>
2023-12-16 14:12:36 +0400

    feat: Lab

---
+++ script.js
@@ -0,0 +1,93 @@
+// // username(String), userAge(Number), isAdmin(Boolean), salary(null), date(undefined)
+
+// // let username = 'Rashad'
+// // let userAge = 18
+// // let isAdmin = true
+// // let salary = null
+// // let data
+
+// // console.log(username);
+// // console.log(userAge);
+// // console.log(isAdmin);
+// // console.log(salary);
+// // console.log(data);
+
+// let userAge = 18;
+// let userName = "Rashad";
+// let isAdmin = true ;
+
+
+// if (userAge = 18) {
+//     console.log('Your allowed');
+// } else if (userName == "Rashad") {
+//     console.log('Name is Rashad');
+// } else if (isAdmin) {
+//     console.log('Ur re admin');
+// } else { 
+// console.log('sorry');
+// } 
+
+
+// switch (userAge) {
+//     case 18:
+//         console.log('ok');
+//         break;
+
+//     default:
+//         console.log('def'); 
+//         break;
+// }
+
+
+// switch (userName= 'Rashad') {
+//     case 'rashad':
+//         console.log('rashad');
+//         break;
+//     case 'ashad':
+//         console.log('ashad');
+//         break; 
+//     case 'Rashad': 
+//         console.log('Rashad');
+//         break; 
+//     case 'Rasad':
+//         console.log('Rasad');   
+//         default:
+//             console.log('def');
+//         break;
+// }
+
+
+
+// switch (userAge = 18) {
+//     case 18:
+//         console.log('You are allowed');
+//         break;
+//     case 19: 
+//         console.log('You are too old');
+//     default: 
+//         console.log('def');
+//     break;
+// }
+// switch (userName = 'Rashad') { 
+//     case 'rashad': 
+//         console.log('Incorrect name');
+//     default:
+//         console.log('Rashad as defoult');
+//         break;
+// }
+// switch (isAdmin) {
+//     case true:
+//         console.log('Your are Admin');
+//         break;
+
+//     default:
+//         console.log('As normal You are not Admin');
+//         break;
+// }
+
+
+
+// // let userAge = 18 
+
+// if (userAge > 14 || userAge > 15 || userAge > 16) 
+//     console.log('You are adult');
