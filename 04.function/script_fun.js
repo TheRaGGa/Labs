ec3a418ff2229b3ce096495d93a3ca416d432ced
TheRaGGa <rashadva@code.edu.az>
2023-12-24 14:53:44 +0400

    feated: functions, html and labs

---
+++ 04.function/script_fun.js
@@ -0,0 +1,88 @@
+
+
+// let age = 17 
+//     age > 20 ? 
+//         console.log("a")
+//            : age == 94 ?     
+//                 console.log("b"): 
+//                 console.log("c");
+
+// let user1 = "Rashad"
+//     if (user1.startsWith === "A") {
+//         console.log("1");
+//     } else { 
+//         console.log("2");
+//     }
+//     if (user1.endsWith === "T") {
+//         console.log("1");
+//     } else {
+//         console.log("2");
+//     }
+
+//     let user2 = "Rashad"
+//         user2.startsWith("A") ? 
+//             console.log("11")
+//                 : user1.endsWith("T") ?
+//                     console.log("21"):   
+//                     console.log("2211");
+            
+
+// function sumNum() {
+//         return 8 + 9 
+//     }
+//         console.log(sumNum());
+
+// function multNum() {
+//     return 19 + 59 + 9
+// }  
+//         console.log(multNum());
+
+
+// const users1 = [1, 4, 6, 7, 10, 19, 20 , 33, 45, 50]
+// const users2 = [1, 4, 6, 7, 10, 19, 780 , 33, 15, 567]
+// const users3 = [1, 4, 6, 7, 10, 19, 20 , 53, 45, 521]
+// const users4 = [1, 4, 6, 7, 10, 19, 20 , 33, 45, 56]
+//         function getAvg(arrNum) {
+//                 let sum = 0
+//                 arrNum.map((user) => sum += user)
+//                 return Math.round(sum / arrNum.length)
+//             }
+//             console.log(getAvg(users1));
+//             console.log(getAvg(users2));
+//             console.log(getAvg(users3));
+//             console.log(getAvg(users4));
+
+// const studentsArr = ["Nurlan", "Laman", "Omer"]
+// function getUsers() { 
+//     return studentsArr.filter((student) => student.startsWith("N"))
+// } 
+//     console.log(getUsers(studentsArr));
+
+
+//     function getUsers2() { 
+//         return studentsArr.filter((student) => student.length === 6) 
+//     }
+//     console.log(getUsers(studentsArr));
+
+    
+
+// function getAvg3() { 
+//     return arrNum.reduce((acc, curr) => acc + curr, 0) / arrNum.length
+// }
+//     console.log(getAvg3());
+
+// for ( i = 0; i<= 20; i++){
+//     console.log(i);
+// }
+
+// const name1 = " Rashad"
+// for ( i = 0; i < name1.length; i++) {
+//     console.log(name1[i]);
+// }
+
+// for ( let i = 0; i <= 20; i++)
+//  {
+    // if (i % 2 == 0) 
+    // console.log(i);
+// }
+ 
