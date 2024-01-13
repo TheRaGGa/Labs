56d33d9fcd3a42ed865325cd72af6991190dfda4
TheRaGGa <rashadva@code.edu.az>
2024-01-13 15:04:10 +0400

    feat:Api

---
+++ 07.api/script.js
@@ -0,0 +1,63 @@
+
+
+
+// const form = document.querySelector('.myform')
+// const username = document.querySelector('#username')
+// const email = document.querySelector('#email')
+// const passwd = document.querySelector('#passwd')
+// const confirmpasswd = document.querySelector('#confirmpasswd')
+// const checkbox = document.querySelector('#checkbox')
+
+// const submitBtn = document.querySelector('#btnSubmit')
+
+
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     const user = {
+//         name: username.value,
+//         email: email.value,
+//         passwd: passwd.value,
+//         confirmpasswd: confirmpasswd.value 
+//     }
+//     if ( confirmpasswd.value !== passwd.value){
+//         alert('Confirm Password is not match Password')
+//         return
+//     } else if (checkbox.checked){
+//         window.location.href = 'user.html';
+//     }
+//     else if (checkbox.checked === false) {
+//         alert('You are user')
+//     }
+//     console.log('user', user);
+// })
+
+
+// function removeDisabled(anyInput) {
+//     anyInput.addEventListener('change', () => {
+//         if (username.value && email.value && passwd.value && confirmpasswd.value ) {
+//             if ( username.value.length > 5 && email.value.length > 5 && passwd.value.length > 5 && confirmpasswd.value.length > 5){
+//                 console.log('removed disabled');
+//                 submitBtn.removeAttribute('disabled')
+//             } else {
+//         submitBtn.setAttribute('disabled', true)
+//     }
+//     } 
+// })
+// }
+
+
+
+// removeDisabled(username)
+// removeDisabled(email)
+// removeDisabled(passwd)
+// removeDisabled(confirmpasswd)
+
+
+// if (username.value && email.value) {
+//     submitBtn.removeAttribute('disabled')
+//     console.log('removed disabled');
+// }
+// console.log('username', username);
+
+
